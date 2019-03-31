d1841c53be2a6071e5aa810beb431e1e53520a6e
heval22 <hb248@njit.edu>
2019-03-31 13:59:23 -0400

    Project Setup - Seeding

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                     Profiles
+                     Profile(s)
                         @foreach ($posts as $post)
                             <h1> {{ $post->title }} </h1>
                                <p> {{ $post->body}} </p>
